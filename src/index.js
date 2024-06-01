1c29976f24de9bd23a2b17a16bc1f47327ee7cbf
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-01 22:29:02 +0500

    feat: implemented the rendering a list in Menu component

--- src/index.js
+++ src/index.js
@@ -80,22 +80,28 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
+      {/* Rendering a list */}
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
       {/* nesting component */}
-      <Pizza
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
         price={10}
-      />
+      /> */}
       {/* reusing the same component */}
-      <Pizza
+      {/* <Pizza
         name="Pizza Funghi"
         ingredients="Tomato, mushrooms"
         price={12}
         photoName="pizzas/funghi.jpg"
-      />
-      {/* <Pizza /> */}
-      {/* <Pizza /> */}
+      /> */}
     </main>
   );
 }
@@ -107,14 +113,14 @@ function Menu() {
 // Pizza component accepts props and uses them to render details about a specific pizza
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name}></img>
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
