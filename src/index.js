d07a0991dd97849034c8ccd8d2e0eb40818a9d9e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-02 22:15:05 +0500

    refactor: added text in Menu component & wrapped into React fragment

--- src/index.js
+++ src/index.js
@@ -86,11 +86,19 @@ function Menu() {
 
       {/* Rendering a list & Conditional Rendering */}
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        // the empty tag is React fragment
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later 😊</p>
       )}
@@ -118,7 +126,7 @@ function Menu() {
 // this component will contain some data about pizza
 // props are like communication channel between parent and child components
 // Pizza component accepts props and uses them to render details about a specific pizza
-// we can directly destructure the 'props', btw the name must be exactly the same
+// we can directly destructure the 'props', btw the name must be exactly the same !
 function Pizza({ pizzaObj }) {
   if (pizzaObj.soldOut) {
     return null;
