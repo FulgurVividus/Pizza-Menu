ca67ac8fd4ecce005f34031310de6227de169f1e
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-02 22:48:20 +0500

    feat: set classes & text conditionally in Pizza component

--- src/index.js
+++ src/index.js
@@ -128,17 +128,17 @@ function Menu() {
 // Pizza component accepts props and uses them to render details about a specific pizza
 // we can directly destructure the 'props', btw the name must be exactly the same !
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) {
-    return null;
-  }
+  // if (pizzaObj.soldOut) {
+  //   return null;
+  // }
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name}></img>
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}$</span>
+        <span>{pizzaObj.soldOut ? `SOLD OUT` : pizzaObj.price + "$"}</span>
       </div>
     </li>
   );
